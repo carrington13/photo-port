b6ecaa60b324e57439f7e93c009b191dc7d654f4
Mediogre <caseyarrington13@gmail.com>
2021-06-21 22:58:25 -0500

    add about component tests/passed

---
+++ src/components/About/_tests_/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+// After each test runs, clear and delete all compenents/data
+afterEach(cleanup);
+// declares the component we're testing
+describe('About component', () => {
+    // First test. Baseline to verify that component is rendering
+    it('renders', () => {
+        render(<About />);
+    });
+    // 2nd test. 
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
